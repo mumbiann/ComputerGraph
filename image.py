116a70f2bf9f6ed0db9e5990e89453396a3ca125
mumbiann <107537497+mumbiann@users.noreply.github.com>
2023-10-12 10:35:45 +0300

    Filling and stroking

--- image.py
+++ image.py
@@ -1,7 +1,7 @@
 import cairo
 
 #set up surface
-surface= cairo.ImageSurface(cairo.FORMAT_RGB24, 600, 400)
+surface= cairo.ImageSurface(cairo.FORMAT_RGB24, 800, 400)
 '''When we create the surface, we are also specifying the type of output.
 An ImageSurface will create a PNG image as output. There are other types of surfaces that create different output formats, for example SVG and PDF.
 We set the format to FORMAT_RGB24, which is a normal RGB image. We set the output image’s size to 600 by 400 pixels
@@ -12,11 +12,12 @@ This is used to create a context. A context is a tool we use to draw on the surf
 We can think of it as being analogous to a pen that is used to draw. However, this is a somewhat weak analogy.
 '''
 ctx.set_source_rgb(0.8, 0.8, 0.8)
-#We will fill the entire surface with a light gray color, using the paint function.
+'''We will fill the entire surface with a light gray color, using the paint function.'''
 ctx.paint()
 
 #Draw the image
-ctx.rectangle(150, 100, 100, 240)
+# red rectangle
+ctx.rectangle(100, 100, 100, 240)
 '''
 The rectangle(x,y,width,height) function adds a rectangle shape to the path.
 It doesn’t draw a rectangle. Instead, it adds it to the plan of what we are intending to draw.
@@ -29,4 +30,24 @@ Call the fill function. This function looks at the path we have defined and fill
 The fill function is what actually draws the shape which, in this case, is a rectangle.
 '''
 ctx.fill()
+
+#Green rectangle
+ctx.rectangle(250, 100, 100, 240) 
+ctx.set_source_rgb(1, 0, 0)
+ctx.fill()
+
+#blue square
+ctx.rectangle(400, 100, 100, 240) 
+ctx.set_source_rgb(0, 1, 0)
+ctx.set_line_width(5)
+ctx.stroke()
+
+#blue square fill and stroke
+ctx.rectangle(550, 100, 100, 240) 
+ctx.set_source_rgb(0, 0, 1)
+ctx.fill_preserve()
+ctx.set_source_rgb(0,0,0)
+ctx.set_line_width(10)
+ctx.stroke()
+
 surface.write_to_png("output.png")
