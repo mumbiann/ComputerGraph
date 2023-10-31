1fdd8b52c4692c9cbe5e2d67c12ebdba3d57ac1a
mumbiann <107537497+mumbiann@users.noreply.github.com>
2023-10-31 16:49:52 +0300

    changes2

--- qn2.py
+++ qn2.py
@@ -1,5 +1,4 @@
 import cairo
-import os
 
 surface = cairo.ImageSurface(cairo. FORMAT_RGB24, 600, 400)
 ctx = cairo.Context(surface)
@@ -7,7 +6,7 @@ ctx.set_source_rgb(0.8, 0.8, 0.8)
 ctx.paint()
 
 ctx.rectangle(150, 100, 100, 240)
-ctx.set_source_rgb(1, 1, 1)
+ctx.set_source_rgb(1, 0.9, 1)
 ctx.fill()
 
 output_dir = "outputs"  
